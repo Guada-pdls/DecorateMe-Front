3922bb11d8252eabd9b8e5c9af94c511a8956d07
Isra QuirozZ <71298156+IsraQuirozZ@users.noreply.github.com>
2023-06-15 22:52:20 +0200

    Quiroz 7 (#4)
    * Complete quiroz-7
    
    * Complete sprint-7

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,34 +1,56 @@
-import { useState } from "react"
-import './Header.css'
+import { useEffect, useState } from "react";
+import axios from "axios";
+import "./Header.css";
 
 const Header = () => {
+  const [open, setOpen] = useState(false);
+  const [products, setProducts] = useState(0);
 
-    const [open, setOpen] = useState(false)
+  useEffect(() => {
+    axios
+      .get(`http://localhost:8080/api/cart/648a0049c5392c5c08014dc6`)
+      .then((res) => {
+        setProducts(res.data.response.products.length);
+      });
+  }, []);
 
   return (
     <header>
       <picture className="logo">
-        <a href="/"><img src="" alt="Logo" /></a>
+        <a href="/">
+          <img src="" alt="Logo" />
+        </a>
       </picture>
       <div>
         <div>
-          <a href="/carts">
-            <i><img src="/icons/shopping-cart.svg" alt="Cart"/></i>
+          <a href="/cart">
+            <i>
+              <img src="/icons/shopping-cart.svg" alt="Cart" />
+            </i>
           </a>
-          <span id="cart">20</span>
+          <span id="cart">{products}</span>
         </div>
         <div>
-          <i onClick={() => setOpen(!open)} id={open ? "open" : "close"}><img src={"/icons/" + (open ? "x" : "bars") + ".svg"} alt={open ? "open menu" : "close menu"}/></i>
+          <i onClick={() => setOpen(!open)} id={open ? "open" : "close"}>
+            <img
+              src={"/icons/" + (open ? "x" : "bars") + ".svg"}
+              alt={open ? "open menu" : "close menu"}
+            />
+          </i>
         </div>
       </div>
-      <nav style={open ? {display: "block"} : {display: "none"}}>
+      <nav style={open ? { display: "block" } : { display: "none" }}>
         <ul>
-          <li><a href="/products">Products</a></li>
-          <li><a href="/chat">Chat</a></li>
+          <li>
+            <a href="/products">Products</a>
+          </li>
+          <li>
+            <a href="/chat">Chat</a>
+          </li>
         </ul>
       </nav>
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
