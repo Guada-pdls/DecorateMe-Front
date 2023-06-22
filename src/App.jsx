22965516f69ba4a539e648c86a5f2818bee92e5f
Israel <isra_quirozz@hotmail.com>
2023-06-22 21:20:22 +0200

    Complete quiroz-8

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import ProductsContainer from "./Components/Products/ProductsContainer";
 import ProductDetail from "./Components/ProductDetail/ProductDetail";
 import Cart from "./Components/Cart/Cart";
 import NewProduct from "./Components/NewProduct/NewProduct";
+import Login from "./Components/Login/Login";
 // import Chat from './Components/Chat/Chat'
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
           <Route path="/products/:pid" element={<ProductDetail />} />
           <Route path="/new_product" element={<NewProduct />} />
           <Route path="/cart" element={<Cart />} />
+          <Route path="/login" element={<Login />}></Route>
           {/* <Route path='/chat' element={<Chat/>}/> */}
         </Routes>
       </BrowserRouter>
