cbf0648acc37f5962fc3fc55573ba2a7bbf13dad
Guada <guadapdls@gmail.com>
2023-10-09 21:10:22 -0300

    users table

--- src/Components/Users/UserListItem.jsx
+++ src/Components/Users/UserListItem.jsx
@@ -4,9 +4,31 @@ import { MenuItem, Select, TableCell, TableRow } from "@mui/material"
 import { useContext } from "react"
 import { UserContext } from "../../Context/UserContext"
 
-const UserListItem = ({ user }) => {
+const UserListItem = ({ user, setUsers, users, setMessage }) => {
 
 	const { updateUser, deleteUser } = useContext(UserContext)
+
+	const currentDate = Date.now()
+	const timeSinceLastConnection = (lastConnection) => {
+
+		if (lastConnection === undefined) return 'Never' 
+
+		const difference = currentDate - lastConnection
+		const seconds = Math.floor(difference / 1000)
+
+		if (seconds < 60) {
+			return 'A few seconds ago'
+		} else if (seconds < 3600) {
+			const minutes = Math.floor(seconds / 60)
+			return `${minutes} minutes ago`
+		} else if (seconds < 86400) {
+			const hours = Math.floor(seconds / 3600)
+			return `${hours} hours ago`
+		} else {
+			const days = Math.floor(seconds / 86400)
+			return `${days} days ago`
+		}
+	}
 
 	const changeHandler = e => {
 		updateUser(user._id, { role: e.target.value })
@@ -16,31 +38,39 @@ const UserListItem = ({ user }) => {
 
 	const clickHandler = () => {
 		deleteUser(user._id)
-		.then(res => console.log(res))
-		.catch(error => console.log(error))
+		.then(res => {
+			setMessage({ success: true, open: true, text: res.data.response })
+			console.log(res.data.response)
+			setUsers(users.filter(dbUser => dbUser._id !== user._id))
+		})
+		.catch(error => {
+			setMessage({ success: false, open: true, text: error.response.data.error})
+		})
 		// TODO: Success or failure message
 	}
 
 	return (
-		<TableRow
-			sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
-		>
-			<TableCell component="th" scope="row">
-				{user.full_name}
-			</TableCell>
-			<TableCell align="right">{user.last_connection}</TableCell>
-			<TableCell align="right">{user.email}</TableCell>
-			<TableCell align="right">
-				<Select onChange={changeHandler} defaultValue={user.role}>
-					<MenuItem value="user">user</MenuItem>
-					<MenuItem value="premium">premium</MenuItem>
-					<MenuItem value="admin">admin</MenuItem>
-				</Select>
-			</TableCell>
-			<TableCell align="right">
-				<Delete onClick={clickHandler} />
-			</TableCell>
-		</TableRow>
+		<>
+			<TableRow
+				sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+			>
+				<TableCell component="th" scope="row">
+					{user.full_name}
+				</TableCell>
+				<TableCell align="right">{timeSinceLastConnection(user.last_connection)}</TableCell>
+				<TableCell align="right">{user.email}</TableCell>
+				<TableCell align="right">
+					<Select onChange={changeHandler} defaultValue={user.role}>
+						<MenuItem value="user">user</MenuItem>
+						<MenuItem value="premium">premium</MenuItem>
+						<MenuItem value="admin">admin</MenuItem>
+					</Select>
+				</TableCell>
+				<TableCell align="right">
+					<Delete onClick={clickHandler} />
+				</TableCell>
+			</TableRow>
+		</>
 	)
 }
 
--- src/Components/Users/Users.jsx
+++ src/Components/Users/Users.jsx
@@ -3,46 +3,51 @@ import { useContext, useEffect, useState } from 'react'
 import { UserContext } from '../../Context/UserContext'
 import UserListItem from './UserListItem'
 import Swal from 'sweetalert2'
+import Toast from '../Toast'
 
 const Users = () => {
 
-	const { user, getUsers } = useContext(UserContext)
-	const [users, setUsers] = useState([])
+  const { user, getUsers } = useContext(UserContext)
+  const [users, setUsers] = useState([])
 
-	useEffect(() => {
-		getUsers()
-			.then(res => {
-				setUsers(res.data.response.users)
-			})
-			.catch(error => {
-				Swal.fire('Error', error.response.data.error, 'error')
-			})
-	}, [])
+  const [message, setMessage] = useState({ open: false })
 
-	return (
-		user.role?.toUpperCase() === "ADMIN" ?
-		(<Typography variant='h4'>Users</Typography>,
-		<TableContainer component={Paper}>
-      <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
-        <TableHead>
-          <TableRow>
-            <TableCell>Name</TableCell>
-            <TableCell align="right">Last connection</TableCell>
-            <TableCell align="right">Email</TableCell>
-            <TableCell align="right">Role</TableCell>
-            <TableCell align="right"></TableCell>
-            {/* <TableCell align="right">Protein&nbsp;(g)</TableCell> */}
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {users.map(user => (
-            <UserListItem key={user._id} user={user}/>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>)
-		: <Typography>Not authorized</Typography>
-	)
+  useEffect(() => {
+    getUsers()
+      .then(res => {
+        setUsers(res.data.response.users)
+      })
+      .catch(error => {
+        Swal.fire('Error', error.response.data.error, 'error')
+      })
+  }, [])
+
+  return (
+    user.role?.toUpperCase() === "ADMIN" ?
+      (<Typography variant='h4'>Users</Typography>,
+        <>
+          <TableContainer component={Paper}>
+            <Table sx={{ minWidth: 650 }} size="small" aria-label="a dense table">
+              <TableHead>
+                <TableRow>
+                  <TableCell>Name</TableCell>
+                  <TableCell align="right">Last connection</TableCell>
+                  <TableCell align="right">Email</TableCell>
+                  <TableCell align="right">Role</TableCell>
+                  <TableCell align="right"></TableCell>
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {users.map(user => (
+                  <UserListItem key={user._id} user={user} setUsers={setUsers} users={users} setMessage={setMessage} />
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <Toast msg={message.text} severity={message.success ? 'success' : 'error'} open={message.open} />
+        </>)
+      : <Typography>Not authorized</Typography>
+  )
 }
 
 export default Users
